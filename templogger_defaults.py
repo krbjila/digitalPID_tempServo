01934532c0366c3150dbbc5e651cda112322bd5f
ldm <krb.jila@gmail.com>
2019-02-04 12:43:00 -0700

    Test commit

--- templogger_defaults.py
+++ templogger_defaults.py
@@ -16,6 +16,7 @@ DEFAULTKD = 400
 ### Set Point [Note: Set point cannot be modified from the GUI]
 DEFAULTSP = 22
 
+
 ### Plotting paramters
 NPTSSHOW = 1440 #Number of points to show
 NPTSDEL = 100 #Number of points to remove at head of graph
